51ae4276463b1be8c29c08a9b44a0a8616f5f9b5
colin.dignazio <colin.dignazio@tippingcanoe.com>
2015-03-27 18:05:43 -0500

    Typo

--- classes/IndexController.php
+++ classes/IndexController.php
@@ -5,7 +5,7 @@ require_once ROOT . 'classes/SkinController.php';
 class IndexController {
 
 	public function init() {
-		$skinCtrl = new SinController();
+		$skinCtrl = new SkinController();
 		$skinCtrl->printSkins();
 		$skins = $skinCtrl->getRandomSkins();
 		echo $skins[0]->name . ' ' . $skins[1]->name;
